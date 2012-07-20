b26ffbee84b393927976fa5505813a13e23f2136
Carson Shold <carson@carsonshold.com>
2012-07-20 10:20:22 -0400

    v1.0
    - First commit.
    - There are definitely some holes in the code and logic, but here are
    the barebones.
    - After authentication, search through the current user's or friend's
    photos (if they give their privacy settings allow it)

--- README.md
+++ README.md
@@ -1,4 +1,62 @@
-jQuery-Facebook-Photo-Selector
-==============================
+jQuery Facebook Photo Selector
+=========================================
 
-A JavaScript plugin to create a Facebook photo selector
+This is an interface component that allows you to select a photo from your albums as if you were inside of Facebook. It mimics the 'Change your profile picture' dialogue, but allows you to customize it as you like.
+
+![This is what it looks like.](http://labs.carsonshold.com/fb-photo-selector/demo.png)
+
+## Viewing the example
+
+You can view the example at [CarsonShold.com](http://labs.carsonshold.com/fb-photo-selector/), or you can check out this repository and run it yourself. You'll need to put the files on a web server or virtual host - Facebook apps won't run off the local file system.
+
+Just edit `example.js` and set your Facebook `appId`.
+
+## Using the plugin
+
+### Include required HTML
+
+- Copy the `div` element with the ID `CSPhotoSelector` (and all of its children) from `index.html`.
+
+### Include required CSS
+
+- Include the `csphotoselector.css` stylesheet in your document.
+- Ensure the `csphotoselector` folder is located in the same directory as `csphotoselector.css`. It holds required images.
+
+### Include required JavaScript
+
+- Include jQuery in your document.
+- Include the Facebook [JavaScript SDK](http://developers.facebook.com/docs/reference/javascript/).
+- Include `csphotoselector.js`.
+
+### The fun stuff (using the plugin)
+
+1 - Make sure your user has authenticated your Facebook app. `your_photos` is required. `friends_photos` is required if you would like to browse a friend's photos.
+
+2 - Initialise the plugin. Here you can set options like toggling debug messages, your preferred classnames, etc.
+
+	CSPhotoSelector.init({debug: true});
+
+3 - Create an instance of the plugin.
+
+	selector = CSPhotoSelector.newInstance({
+		callbackAlbumSelected	: callbackAlbumSelected,
+		callbackAlbumUnselected	: callbackAlbumUnselected,
+		callbackPhotoSelected	: callbackPhotoSelected,
+		callbackPhotoUnselected	: callbackPhotoUnselected,
+		callbackSubmit			: callbackSubmit,
+		maxSelection			: 1,
+		albumsPerPage			: 6,
+		photosPerPage			: 200,
+		autoDeselection			: true
+	});
+
+4 - Display the plugin instance when you need it. The plugin will automatically load the Facebook photos of the logged in user, or attempt to load the photos of the friend ID passed to the function.
+
+	$(".photoSelect").click(function (e) {
+		e.preventDefault();
+		id = null;
+		if ( $(this).attr('data-id') ) id = $(this).attr('data-id');
+		fbphotoSelect(id);
+	});
+
+
---
+++ example.js
@@ -0,0 +1,103 @@
+/*globals $, jQuery, CSPhotoSelector */
+
+$(document).ready(function () {
+	var selector, logActivity, callbackAlbumSelected, callbackPhotoUnselected, callbackSubmit;
+	var buttonOK = $('#CSPhotoSelector_buttonOK');
+	var o = this;
+	
+	
+	/* --------------------------------------------------------------------
+	 * Photo selector functions
+	 * ----------------------------------------------------------------- */
+	
+	fbphotoSelect = function(id) {
+		// if no user/friend id is sent, default to current user
+		if (!id) id = 'me';
+		
+		callbackAlbumSelected = function(albumId) {
+			var album, name;
+			album = CSPhotoSelector.getAlbumById(albumId);
+			// show album photos
+			selector.showPhotoSelector(null, album.id);
+		};
+
+		callbackAlbumUnselected = function(albumId) {
+			var album, name;
+			album = CSPhotoSelector.getAlbumById(albumId);
+		};
+
+		callbackPhotoSelected = function(photoId) {
+			var photo;
+			photo = CSPhotoSelector.getPhotoById(photoId);
+			buttonOK.show();
+			logActivity('Selected ID: ' + photo.id);
+		};
+
+		callbackPhotoUnselected = function(photoId) {
+			var photo;
+			album = CSPhotoSelector.getPhotoById(photoId);
+			buttonOK.hide();
+		};
+
+		callbackSubmit = function(photoId) {
+			var photo;
+			photo = CSPhotoSelector.getPhotoById(photoId);
+			console.log(photo);
+			logActivity('<br><strong>Submitted</strong><br> Photo ID: ' + photo.id + '<br>Photo URL: ' + photo.source + '<br>');
+		};
+
+
+		// Initialise the Photo Selector with options that will apply to all instances
+		CSPhotoSelector.init({debug: true});
+
+		// Create Photo Selector instances
+		selector = CSPhotoSelector.newInstance({
+			callbackAlbumSelected	: callbackAlbumSelected,
+			callbackAlbumUnselected	: callbackAlbumUnselected,
+			callbackPhotoSelected	: callbackPhotoSelected,
+			callbackPhotoUnselected	: callbackPhotoUnselected,
+			callbackSubmit			: callbackSubmit,
+			maxSelection			: 1,
+			albumsPerPage			: 6,
+			photosPerPage			: 200,
+			autoDeselection			: true
+		});
+
+		// reset and show album selector
+		selector.reset();
+		selector.showAlbumSelector(id);
+	}
+	
+	
+	/* --------------------------------------------------------------------
+	 * Click events
+	 * ----------------------------------------------------------------- */
+	
+	$("#btnLogin").click(function (e) {
+		e.preventDefault();
+		FB.login(function (response) {
+			if (response.authResponse) {
+				$("#login-status").html("Logged in");
+			} else {
+				$("#login-status").html("Not logged in");
+			}
+		}, {scope:'user_photos, friends_photos'});
+	});
+	
+	$("#btnLogout").click(function (e) {
+		e.preventDefault();
+		FB.logout();
+		$("#login-status").html("Not logged in");
+	});
+	
+	$(".photoSelect").click(function (e) {
+		e.preventDefault();
+		id = null;
+		if ( $(this).attr('data-id') ) id = $(this).attr('data-id');
+		fbphotoSelect(id);
+	});
+
+	logActivity = function (message) {
+		$("#results").append('<div>' + message + '</div>');
+	};
+});
